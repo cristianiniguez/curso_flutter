0a4151350b1c3022502c114086b65f1c485dfdfd
cristianiniguez <felizpc1@gmail.com>
2021-05-23 18:30:17 -0400

    widgets sin estado en flutter: widgets decorados

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'review_list.dart';
 import 'description_place.dart';
+import 'gradient_back.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,8 +14,18 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(primarySwatch: Colors.blue),
       home: Scaffold(
-        appBar: AppBar(title: Text("Hola Mundo Feliz")),
-        body: DescriptionPlace("Bahamas", 4, "lLorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce dictum in est id euismod. Vivamus tempus viverra lectus sit amet interdum. Quisque nibh sem, porttitor quis aliquet a, mollis et lorem."),
+        body: Stack(
+          children: [
+            ListView(
+              children: [
+                DescriptionPlace("Bahamas", 4,
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce dictum in est id euismod. Vivamus tempus viverra lectus sit amet interdum. Quisque nibh sem, porttitor quis aliquet a, mollis et lorem."),
+                ReviewList()
+              ],
+            ),
+            GradientBack()
+          ],
+        ),
       ),
     );
   }
